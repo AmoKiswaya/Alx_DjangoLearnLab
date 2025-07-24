5b381d0cee839ae0af6c351f4b9e6e589586340a
AmoKiswaya <amosonganga254@gmail.com>
2025-07-24 12:54:45 +0300

    Create custom manager.

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Book 
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy
+from .models import CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year' )
@@ -7,3 +10,25 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
 
 admin.site.register(Book, BookAdmin) 
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # Add custom fields to the default fieldsets for editing existing users
+    fieldsets = UserAdmin.fieldsets + (
+        (gettext_lazy('Additional Info'), {
+            'fields': ('date_of_birth', 'profile_photo')
+        }),
+    )
+
+    # Add custom fields when creating a new user in admin
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (gettext_lazy('Additional Info'), {
+            'fields': ('date_of_birth', 'profile_photo')
+        }),
+    )
+
+    # Fields to display in list view
+    list_display = ('username', 'email', 'first_name', 'last_name', 'date_of_birth', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
