8833932d62dabacfe9102c9d26d86463309d0356
AmoKiswaya <amosonganga254@gmail.com>
2025-07-30 20:25:07 +0300

    Add authentication and permission layers to existing API endpoints.

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import include
 from .views import BookList
 from .views import BookViewSet
 from rest_framework.routers import DefaultRouter 
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all') 
@@ -10,5 +11,7 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  
      path('', include(router.urls)),
+     path('auth/token/', obtain_auth_token, name='api-token-auth'),
 ]
+
 
