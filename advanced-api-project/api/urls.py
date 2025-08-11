49c8053aec424491e5545ea86bf1f384654edb2a
AmoKiswaya <amosonganga254@gmail.com>
2025-08-11 11:37:41 +0300

    Update url for UpdateView.

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('books/', ListView.as_view(), name='list-books'),
     path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),
     path('books/create/', CreateView.as_view(), name='create-book'),
-    path('books/<int:pk>/update/', UpdateView.as_view(), name='update-book'),
+    path('books/update/', UpdateView.as_view(), name='update-book'),
     path('books/<int:pk>/delete/', DeleteView.as_view(), name='delete-book'),
     path('api/auth/token/', obtain_auth_token, name='api_token_auth'),
 ]
