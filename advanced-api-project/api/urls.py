54bf6cc14b330ae245b678852dda2688208bcb92
AmoKiswaya <amosonganga254@gmail.com>
2025-08-11 11:34:45 +0300

    Construct custom views and utilize generic views in Django REST Framework.

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import ListView
+from .views import DetailView
+from .views import DeleteView
+from .views import CreateView
+from .views import UpdateView
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('books/', ListView.as_view(), name='list-books'),
+    path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),
+    path('books/create/', CreateView.as_view(), name='create-book'),
+    path('books/<int:pk>/update/', UpdateView.as_view(), name='update-book'),
+    path('books/<int:pk>/delete/', DeleteView.as_view(), name='delete-book'),
+    path('api/auth/token/', obtain_auth_token, name='api_token_auth'),
+]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.exceptions import ValidationError
+from django.utils import timezone
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class ListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        current_year = timezone.now().year
+        if serializer.validated_data['publication_year'] > current_year:
+            raise ValidationError({"publication_year": "publication year cannot be in the future"})
+        serializer.save() 
+
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        current_year = timezone.now().year
+        if serializer.validated_data['publication_year'] > current_year:
+            raise ValidationError({"publication_year": "publication year cannot be in the future"})
+        serializer.save() 
+
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated] 
