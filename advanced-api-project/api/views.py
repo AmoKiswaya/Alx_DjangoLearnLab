1b70cd0918058f240ed6f884376a1927078a7f45
AmoKiswaya <amosonganga254@gmail.com>
2025-08-11 12:47:37 +0300

    Change import statement for filters to fit checker requirements.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 from .models import Book
 from .serializers import BookSerializer 
 from django_filters import rest_framework
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend   
 
 
@@ -13,7 +13,10 @@ class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter,
+                       filters.OrderingFilter]
+    
     filterset_fields = ['title', 'author', 'publication_year'] 
     search_fields = ['title', 'author'] 
     ordering_fields = ['title', 'publication_year']
