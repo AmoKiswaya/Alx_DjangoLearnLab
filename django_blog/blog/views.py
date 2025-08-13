a109eac34d54af382e54a4a5755319c4fd9c7a98
AmoKiswaya <amosonganga254@gmail.com>
2025-08-13 15:03:01 +0300

    Implement CRUD functionality for posts
    - Added Create, Read, Update, and Delete views for Post model
    - Created templates for each CRUD operation (list, detail, form, delete confirmation)
    - Configured URL patterns for all CRUD views in blog/urls.py
    - Linked templates to URLs using Django's {% url %} template tag

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import CustomUserCreationForm, UserUpdateForm, ProfileUpdateForm 
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 
 def register(request):
     if request.method == 'POST':
@@ -30,4 +34,53 @@ def profile(request):
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
-    return render(request, 'registration/profile.html', {'u_form': u_form, 'p_form': p_form})
+    return render(request, 'blog/profile.html', {'u_form': u_form, 'p_form': p_form})
+
+#Show all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+#Show a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+#Create new post
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content'] 
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+#Edit a post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content'] 
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form) 
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author 
+    
+#Delete a post
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list') 
+
+    def test_func(self):
+        post = self.get_object() 
+        return self.request.user == post.author
+    
+
+    
+    
