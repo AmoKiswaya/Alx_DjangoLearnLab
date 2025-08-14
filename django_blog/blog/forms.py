e3165f5a793b7bdc302c8c3faaf68e4cf5c9b061
AmoKiswaya <amosonganga254@gmail.com>
2025-08-14 17:41:52 +0300

    Remove arguments from TagWidget method.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -12,7 +12,7 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
-            'tags': TagWidget(attrs={'class': 'form-control', 'placeholder': 'Comma-separated tags'}),
+            'tags': TagWidget(),
         } 
 
 
