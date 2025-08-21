79925c341dd4a62be5b869b2cbc81901e9d69bea
AmoKiswaya <amosonganga254@gmail.com>
2025-08-21 23:00:57 +0300

    Create new posts app for managing posts and engaging them through comments.

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import UserRegistrationView, UserLoginView, UserProfileView
+from django.conf.urls.static import static
+from django.conf import settings 
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
